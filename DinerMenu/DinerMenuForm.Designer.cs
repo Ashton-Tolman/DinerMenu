df7d7ed90e5e8406a6e820577534557a90412c19
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-03-29 19:31:50 -0600

    Make buttons work. Need to add text

--- DinerMenu/DinerMenuForm.Designer.cs
+++ DinerMenu/DinerMenuForm.Designer.cs
@@ -32,66 +32,86 @@
             SoupButton = new Button();
             FishButton = new Button();
             ExitButton = new Button();
-            label1 = new Label();
+            DinerTopLabel = new Label();
+            DinerSpecialsLabel = new Label();
             SuspendLayout();
             // 
             // SaladButton
             // 
+            SaladButton.Font = new Font("Stencil", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             SaladButton.Location = new Point(205, 344);
             SaladButton.Name = "SaladButton";
             SaladButton.Size = new Size(130, 70);
             SaladButton.TabIndex = 1;
             SaladButton.Text = "Salad";
             SaladButton.UseVisualStyleBackColor = true;
+            SaladButton.Click += SaladButton_Click;
             // 
             // SoupButton
             // 
+            SoupButton.Font = new Font("Stencil", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             SoupButton.Location = new Point(69, 344);
             SoupButton.Name = "SoupButton";
             SoupButton.Size = new Size(130, 70);
             SoupButton.TabIndex = 0;
             SoupButton.Text = "Soup";
             SoupButton.UseVisualStyleBackColor = true;
+            SoupButton.Click += SoupButton_Click;
             // 
             // FishButton
             // 
+            FishButton.Font = new Font("Stencil", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FishButton.Location = new Point(341, 344);
             FishButton.Name = "FishButton";
             FishButton.Size = new Size(130, 70);
             FishButton.TabIndex = 2;
             FishButton.Text = "Fish";
             FishButton.UseVisualStyleBackColor = true;
+            FishButton.Click += FishButton_Click;
             // 
             // ExitButton
             // 
+            ExitButton.Font = new Font("Stencil", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             ExitButton.Location = new Point(603, 344);
             ExitButton.Name = "ExitButton";
             ExitButton.Size = new Size(130, 70);
             ExitButton.TabIndex = 3;
             ExitButton.Text = "E&xit";
             ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
             // 
-            // label1
+            // DinerTopLabel
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(440, 90);
-            label1.Name = "label1";
-            label1.Size = new Size(59, 25);
-            label1.TabIndex = 4;
-            label1.Text = "label1";
+            DinerTopLabel.AutoSize = true;
+            DinerTopLabel.Font = new Font("Stencil", 16F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            DinerTopLabel.Location = new Point(175, 9);
+            DinerTopLabel.Name = "DinerTopLabel";
+            DinerTopLabel.Size = new Size(379, 38);
+            DinerTopLabel.TabIndex = 4;
+            DinerTopLabel.Text = "RoboCop's Robo Stop";
+            // 
+            // DinerSpecialsLabel
+            // 
+            DinerSpecialsLabel.AutoSize = true;
+            DinerSpecialsLabel.Location = new Point(305, 66);
+            DinerSpecialsLabel.Name = "DinerSpecialsLabel";
+            DinerSpecialsLabel.Size = new Size(117, 25);
+            DinerSpecialsLabel.TabIndex = 5;
+            DinerSpecialsLabel.Text = "DinerSpecials";
             // 
             // DinerMenuForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            Controls.Add(DinerSpecialsLabel);
+            Controls.Add(DinerTopLabel);
             Controls.Add(ExitButton);
             Controls.Add(FishButton);
             Controls.Add(SoupButton);
             Controls.Add(SaladButton);
             Name = "DinerMenuForm";
-            Text = "Form1";
+            Text = "Robo cop's Diner Menu";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -102,6 +122,7 @@
         private Button SoupButton;
         private Button FishButton;
         private Button ExitButton;
-        private Label label1;
+        private Label DinerTopLabel;
+        private Label DinerSpecialsLabel;
     }
 }
--- DinerMenu/DinerMenuForm.cs
+++ DinerMenu/DinerMenuForm.cs
@@ -1,10 +1,42 @@
+
+
+
 namespace DinerMenu
 {
     public partial class DinerMenuForm : Form
     {
         public DinerMenuForm()
+
+        //Defalut Methods------------------------------------------------------
         {
             InitializeComponent();
+            SetDefaults();
+        }
+
+        //Custom Methods-------------------------------------------------------
+        private void SetDefaults()
+        {
+            DinerSpecialsLabel.Text = "";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SoupButton_Click(object sender, EventArgs e)
+        {
+            DinerSpecialsLabel.Text = "Soup";
+        }
+
+        private void SaladButton_Click(object sender, EventArgs e)
+        {
+            DinerSpecialsLabel.Text = "Salad";
+        }
+
+        private void FishButton_Click(object sender, EventArgs e)
+        {
+            DinerSpecialsLabel.Text = "Fish";
         }
     }
 }
