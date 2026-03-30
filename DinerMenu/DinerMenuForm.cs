fbeda75095f9c3244c923b2250db6477ee0f4233
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-03-29 19:51:14 -0600

    Comments

--- DinerMenu/DinerMenuForm.cs
+++ DinerMenu/DinerMenuForm.cs
@@ -1,4 +1,8 @@
-
+/* Ashton Tolman
+ * Spring 2026
+ * DinerMenu
+ * https://github.com/Ashton-Tolman/DinerMenu.git
+ */
 
 
 namespace DinerMenu
@@ -25,6 +29,7 @@ namespace DinerMenu
             this.Close();
         }
 
+        //Displays Soup special
         private void SoupButton_Click(object sender, EventArgs e)
         {
             DinerSpecialsLabel.Text = "~Todays Special Soup~"+
@@ -33,6 +38,7 @@ namespace DinerMenu
                 "\nfuses, transistors, and small IC's";
         }
 
+        //Displays Salad special
         private void SaladButton_Click(object sender, EventArgs e)
         {
             DinerSpecialsLabel.Text = "~Todays Special Salad~" +
@@ -40,6 +46,7 @@ namespace DinerMenu
                 "\nwith bolts and servos on top";
         }
 
+        //Displays Fish special
         private void FishButton_Click(object sender, EventArgs e)
         {
             DinerSpecialsLabel.Text = "~Todays Special Fish~" +
